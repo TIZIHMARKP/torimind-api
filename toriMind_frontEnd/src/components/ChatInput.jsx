600c003d59c8a2446fbaa14a12499080aae9debe
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-09-15 11:42:37 +0100

    Update: Created the chatInput field and submit button

---
+++ toriMind_frontEnd/src/components/ChatInput.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+const ChatInput = ( onSubmit ) => {
+
+    const [question, setQuestion] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(question.trim()){
+            onSubmit(question);
+            setQuestion("");
+        }
+    }
+
+
+  return (
+    <div className='container my-4'>
+      <div onSubmit={handleSubmit} >
+        <label htmlFor="question">Ask a Question</label>
+        
+        <input type="text" 
+            className='form-control'
+            id='question'
+            placeholder='Chat with ToriMind'
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+        />
+      </div>
+
+      <button type='submit' className="btn btn-primary mt-2">
+        Submit
+      </button>
+
+    </div>
+  )
+}
+
+export default ChatInput
